149e6a8a0c31621a46c5016beccbfa2eb8f233ab
Swanjalisingh <2k24.csai1d.2413113@gmail.com>
2026-06-08 23:37:40 +0530

    Time: 5 ms (73.56%) | Memory: 159.7 MB (79.84%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,30 +1,63 @@
+// brute force; 
+// class Solution {
+//     public int[] pivotArray(int[] nums, int pivot) {
+//         List<Integer> less = new ArrayList<>();
+//         List<Integer> equal = new ArrayList<>();
+//         List<Integer>  greater = new ArrayList<>();
+
+//         for(int num : nums){
+//             if(num <pivot)
+//             less.add(num);
+//            else if(num==pivot)
+//             equal.add(num);
+//             else 
+//             greater.add(num);
+//         }
+        
+//         int[] ans = new int[nums.length];
+//         int k = 0;
+
+//         for (int x : less) ans[k++] = x;
+//         for (int x : equal) ans[k++] = x;
+//         for (int x : greater) ans[k++] = x;
+
+//         return ans;
+//     }
+// }
+
+
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        List<Integer> less = new ArrayList<>();
-        List<Integer> equal = new ArrayList<>();
-        List<Integer>  greater = new ArrayList<>();
-
+        int n = nums.length;
+        int countless =0;
+        int countequal =0;
         for(int num : nums){
-            if(num <pivot)
-            less.add(num);
-           else if(num==pivot)
-            equal.add(num);
-            else 
-            greater.add(num);
-
-
-
+            if(num < pivot)
+            countless++;
+            else if(num == pivot)
+            countequal++;
         }
-        
-        int[] ans = new int[nums.length];
-        int k = 0;
+         int i =0;
+         int j = countless;
+         int k = countless + countequal;
+         int[] result = new int[n];
+         
+          for(int num : nums){
+            if(num < pivot){
+                  result[i] = num;
+                  i++;
 
-        for (int x : less) ans[k++] = x;
-        for (int x : equal) ans[k++] = x;
-        for (int x : greater) ans[k++] = x;
+            }
+            else if ( num == pivot){
+                result[j] = num;
+                j++;
+            }else{
+                result[k] = num;
+                k++;
 
-        return ans;
-              
-
+            }
+          }
+           
+           return result;
     }
 }
