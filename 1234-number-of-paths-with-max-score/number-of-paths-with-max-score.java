041a76be25b334cf92df5fefdabc0c36313d3f29
Swanjalisingh <2k24.csai1d.2413113@gmail.com>
2026-07-05 21:58:39 +0530

    Time: 13 ms (67.06%) | Memory: 47.2 MB (22.35%) - LeetSync

---
+++ 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int[] pathsWithMaxScore(List<String> board) {
+        int n = board.size();
+        int MOD = 1_000_000_007;
+
+        int[][] score = new int[n][n];
+        int[][] ways = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(score[i], -1);
+        }
+
+        score[n - 1][n - 1] = 0;
+        ways[n - 1][n - 1] = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+
+                if (board.get(i).charAt(j) == 'X')
+                    continue;
+
+                if (i == n - 1 && j == n - 1)
+                    continue;
+
+                int maxScore = -1;
+                int count = 0;
+
+                int[][] dirs = {{1, 0}, {0, 1}, {1, 1}};
+
+                for (int[] d : dirs) {
+                    int ni = i + d[0];
+                    int nj = j + d[1];
+
+                    if (ni < n && nj < n && score[ni][nj] != -1) {
+                        if (score[ni][nj] > maxScore) {
+                            maxScore = score[ni][nj];
+                            count = ways[ni][nj];
+                        } else if (score[ni][nj] == maxScore) {
+                            count = (count + ways[ni][nj]) % MOD;
+                        }
+                    }
+                }
+
+                if (maxScore == -1)
+                    continue;
+
+                char c = board.get(i).charAt(j);
+                int val = (c == 'E' || c == 'S') ? 0 : c - '0';
+
+                score[i][j] = maxScore + val;
+                ways[i][j] = count;
+            }
+        }
+
+        if (ways[0][0] == 0)
+            return new int[]{0, 0};
+
+        return new int[]{score[0][0], ways[0][0]};
+    }
+}
