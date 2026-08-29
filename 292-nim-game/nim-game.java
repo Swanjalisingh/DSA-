7e738918d837a671a8cc777374467167e054d150
Swanjalisingh <2k24.csai1d.2413113@gmail.com>
2026-08-29 21:55:44 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (54.52%) - LeetSync

---
+++ 292-nim-game/nim-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canWinNim(int n) {
+       if(n==1){
+        return true;
+       }
+       if(n%4 != 0){
+        return true;
+       }
+       return false;
+    }
+}
