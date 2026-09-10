b26a4be5309f5887c55160317e967714759b6295
Swanjalisingh <2k24.csai1d.2413113@gmail.com>
2026-09-10 10:17:22 +0530

    Time: 7 ms (17.08%) | Memory: 25.9 MB (15.79%) - LeetSync

--- 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.cpp
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.cpp
@@ -12,25 +12,11 @@
 class Solution {
     map<int, int> mp;
     vector<int> ans;
+    int maxFreq = 0;
 
 public:
     vector<int> findMode(TreeNode* root) {
         inorder(root);
-
-        int maxFreq = 0;
-
-        // Find maximum frequency
-        for (auto it : mp) {
-            maxFreq = max(maxFreq, it.second);
-        }
-
-        // Store all modes
-        for (auto it : mp) {
-            if (it.second == maxFreq) {
-                ans.push_back(it.first);
-            }
-        }
-
         return ans;
     }
 
@@ -41,6 +27,15 @@ public:
         inorder(root->left);
 
         mp[root->val]++;
+
+        if (mp[root->val] > maxFreq) {
+            maxFreq = mp[root->val];
+            ans.clear();
+            ans.push_back(root->val);
+        }
+        else if (mp[root->val] == maxFreq) {
+            ans.push_back(root->val);
+        }
 
         inorder(root->right);
     }
